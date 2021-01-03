d7852d81dc2c39469893077f69769ee4e24268fd
lamiasristy <lamiasristy@gmail.com>
2021-01-03 01:19:37 +0100

    Added linters

--- webpack.config.js
+++ webpack.config.js
@@ -5,29 +5,29 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'production',
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
   },
 
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
   },
 
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
-        { from: path.resolve(__dirname, 'index.html'), to: path.resolve(__dirname, 'build')},
-        { from: path.resolve(__dirname, 'assets'), to: path.resolve(__dirname, 'build/assets')}
+        { from: path.resolve(__dirname, 'index.html'), to: path.resolve(__dirname, 'build') },
+        { from: path.resolve(__dirname, 'assets'), to: path.resolve(__dirname, 'build/assets') },
       ],
     }),
 
     new webpack.DefinePlugin({
       'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true)
-    }),   
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
   ],
-  
+
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
-  }
-}
+  },
+};
